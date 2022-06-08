9d32f1644ae02c22b584076ea03f15b1ef4d5ca6
Iz <101628793+RFaizal@users.noreply.github.com>
2022-06-08 12:25:09 +0700

    Add files via upload

---
+++ PrinterWindows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    public class PrinterWindows
+    {
+        public string printer { get; set; }
+
+        public virtual void Show()
+        {
+            Console.WriteLine();
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine();
+        }
+    }
+}
